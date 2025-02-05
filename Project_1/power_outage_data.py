88f558fa2f8d651c8106489241979c94db2c4674
Sam Kortchmar <samkortchmar@gmail.com>
2025-02-04 22:15:20 -0500

    added example notebook

--- Project_1/power_outage_data.py
+++ Project_1/power_outage_data.py
@@ -1,4 +1,5 @@
 """Helper script to download and shrink power outage data for easy processing."""
+import json
 import os
 import pandas as pd
 import numpy as np
@@ -70,7 +71,7 @@ def shrink_csv(path):
 
 def shrink_csvs():
     did_shrink = False
-    for year in range(2014, 2023):
+    for year in range(2014, 2024):
         input_path = f'eaglei_outages_{year}.csv'
         output_path = f"eaglei_outages_{year}_filtered.npz"
         if os.path.exists(DATA_DIR + output_path):
@@ -86,13 +87,54 @@ def shrink_csvs():
 
 def load_yearly_data():
     dfs_by_year = {}
-    for year in range(2014, 2023):
+    for year in range(2014, 2024):
         output_path = f"eaglei_outages_{year}_filtered.npz"
         npz = np.load(DATA_DIR + output_path, allow_pickle=True)
         df = pd.DataFrame({key: npz[key] for key in npz.keys()})
         dfs_by_year[year] = df
     
     return dfs_by_year
+
+def load_fips_shapes():
+    filepath = DATA_DIR + 'geojson-counties-fips.json'
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as f:
+            return json.load(f)
+
+    response = requests.get('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json')
+    with open(filepath, 'w') as f:
+        f.write(response.text)
+
+    return response.json()
+
+def point_in_polygon(lat, lon, polygon):
+    n = len(polygon)
+    inside = False
+    p1x, p1y = polygon[0]
+    for i in range(n + 1):
+        p2x, p2y = polygon[i % n]
+        if lat > min(p1y, p2y):
+            if lat <= max(p1y, p2y):
+                if lon <= max(p1x, p2x):
+                    if p1y != p2y:
+                        xinters = (lat - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                    if p1x == p2x or lon <= xinters:
+                        inside = not inside
+        p1x, p1y = p2x, p2y
+
+    return inside
+
+def lat_lon_to_fips(lat, lon, fips_shapes):
+    for feature in fips_shapes['features']:
+        if feature['geometry']['type'] == 'MultiPolygon':
+            for polygon in feature['geometry']['coordinates']:
+                if any([point_in_polygon(lat, lon, poly) for poly in polygon]):
+                    return feature
+        else:
+            if any([point_in_polygon(lat, lon, poly) for poly in feature['geometry']['coordinates']]):
+                return feature
+
+    return None
 
 
 def main():
